f4602f2503d2d1f9c00470363ab50ffbb1378d03
Michael Ebens <michael@nova-fusion.com>
2011-04-03 11:20:48 +1000

    Added __index and __newindex.

--- Vault.lua
+++ Vault.lua
@@ -2,35 +2,8 @@ Vault = {}
 Vault.file = 'data'
 Vault.fileData = {}
 
-function Vault._getTableString(t)
-  local ret = '{'
-  
-  for k, v in pairs(t) do
-    ret = string.format('%s[%s] = %s,', ret, Vault._getString(k), Vault._getString(v))
-  end
-  
-  return ret .. '}'
-end
-
-function Vault._getString(value)
-  local t = type(value)
-  
-  if t == 'nil' then
-    return 'nil'
-  elseif t == 'number' then
-    return tostring(value)
-  elseif t == 'string' then
-    return '"' .. value .. '"'
-  elseif t == 'boolean' then
-    return value == true and 'true' or 'false'
-  elseif t == 'table' then
-    return Vault._getTableString(value)
-  else
-    -- is there a way to do userdata?
-    error('Unable to save type: ' + t)
-  end
-end
-
+-- loads the data from the data file
+-- returns the data
 function Vault.load(file)
   file = file or Vault.file
   
@@ -44,6 +17,9 @@ function Vault.load(file)
   end
 end
 
+-- gets the data from the data, however it caches it,
+-- and doesn't reload it every time
+-- returns the data
 function Vault.data(file)
   file = file or Vault.file
   
@@ -84,3 +60,44 @@ function Vault.save(data, file)
   
   return love.filesystem.write(file, Vault._getString(Vault.fileData[file]))
 end
+
+-- this allows for Vault['key']
+function Vault.__index(_, key)
+  return Vault.get(key)
+end
+
+-- this allows for Vault['key'] = value
+function Vault.__newindex(_, key, value)
+  return Vault.set(key, value)
+end
+
+function Vault._getTableString(t)
+  local ret = '{'
+  
+  for k, v in pairs(t) do
+    ret = string.format('%s[%s] = %s,', ret, Vault._getString(k), Vault._getString(v))
+  end
+  
+  return ret .. '}'
+end
+
+function Vault._getString(value)
+  local t = type(value)
+  
+  if t == 'nil' then
+    return 'nil'
+  elseif t == 'number' then
+    return tostring(value)
+  elseif t == 'string' then
+    return '"' .. value .. '"'
+  elseif t == 'boolean' then
+    return value == true and 'true' or 'false'
+  elseif t == 'table' then
+    return Vault._getTableString(value)
+  else
+    -- is there a way to do userdata?
+    error('Unable to save type: ' + t)
+  end
+end
+
+setmetatable(Vault, Vault)
